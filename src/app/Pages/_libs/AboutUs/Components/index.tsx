c5c2980397c37a2314be879cc80bc14e0d04b7d3
airmjay <abdulmojeedaliyu@gmail.com>
2025-04-07 23:13:37 +0100

    image change height

--- src/app/Pages/_libs/AboutUs/Components/index.tsx
+++ src/app/Pages/_libs/AboutUs/Components/index.tsx
@@ -8,7 +8,7 @@ export const AboutReviewCard: React.FC<AboutType> = ({image,content,name}) => (
     <Box bd={'1px solid #344054'} p='16px' className="rounded-lg">
     <Grid gutter={'lg'}>
      <GridCol span={{base: 12, md: 6}}>
-     <Image className="w-['auto'] h-['auto'] md:w-[239px] md:h-[239px]  sm:w-[100%]" src={image} alt='Abouts us' width={10000} height={10000}/>
+     <Image className="w-['auto'] h-['auto'] md:w-[239px] md:h-[239px]" src={image} alt='Abouts us' width={10000} height={10000}/>
      </GridCol>
      <GridCol span={{base: 12, md: 6}}>
      <Box>
