6b0f607a79537cb9b86a824e4b4fd20f863bcc53
airmjay <abdulmojeedaliyu@gmail.com>
2025-04-07 23:09:33 +0100

    make couple of changes on images

--- src/app/Pages/_libs/OurBlog/page.tsx
+++ src/app/Pages/_libs/OurBlog/page.tsx
@@ -6,44 +6,47 @@ import { OurBlogsCard, OurBlogType } from "@/app/constants/OurBlog/content";
 import { Box, Text } from "@mantine/core";
 import Image from "next/image";
 
-const OurBlogCard:  React.FC<OurBlogType> = ({image,heading,content}) => (
-  <Box w={'100%'}>
-  <Image
-    style={{ height: "350px", width: "100%" }}
-    src={image}
-    alt="our blog 1"
-    width={10000}
-    height={10000}
-    className="rounded-xl"
-  />
-  <Box w={'100%'} mt={'24px'}>
-      <Text fz={'24px'} fw={'500'} lh={'32px'} lts={'0'}>
-      {heading}
+const OurBlogCard: React.FC<OurBlogType> = ({ image, heading, content }) => (
+  <Box w={"100%"}>
+    <Image
+      style={{
+        width: "100%",
+        height: "auto",
+      }}
+      src={image}
+      alt="our blog 1"
+      width={10000}
+      height={10000}
+      className="rounded-xl"
+    />
+    <Box w={"100%"} mt={"24px"}>
+      <Text fz={"24px"} fw={"500"} lh={"32px"} lts={"0"}>
+        {heading}
       </Text>
-      <Text c={'#98A2B3'} fw={'400'} lh={'24px'} lts={'0'} mt={'8px'}>
-      {content} 
+      <Text c={"#98A2B3"} fw={"400"} lh={"24px"} lts={"0"} mt={"8px"}>
+        {content} 
       </Text>
+    </Box>
   </Box>
-  </Box>
-)
+);
 export default function OurBlog() {
   return (
     <div className="p-[20px] md:p-[80px] bg-[#011929] text-gray-100">
-    <Heading text={contentLayout[0].heading as string} />
-    <Box>  
-      <div className="flex gap-[10px] flex-wrap md:flex-nowrap mt-2 md:mt-0 justify-between items-center">
-        <BigHeading text={contentLayout[1].content as string}/>
-        <Text className="flex gap-[10px] items-center">
-          <LeftArrowShift />
-          <RightArrowShift />
-        </Text>
-      </div>
-      <div className="flex flex-wrap gap-[40px] lg:flex-nowrap w-[100%] mt-[20px] md:mt-[68px]">
-        {OurBlogsCard.map((item)=> (
-          <OurBlogCard key={item.id} {...item} />
-        ))}
-      </div>
-    </Box>
+      <Heading text={contentLayout[0].heading as string} />
+      <Box>
+        <div className="flex gap-[10px] flex-wrap md:flex-nowrap mt-2 md:mt-0 justify-between items-center">
+          <BigHeading text={contentLayout[1].content as string} />
+          <Text className="flex gap-[10px] items-center">
+            <LeftArrowShift />
+            <RightArrowShift />
+          </Text>
+        </div>
+        <div className="flex flex-wrap gap-[40px] lg:flex-nowrap w-[100%] mt-[20px] md:mt-[68px]">
+          {OurBlogsCard.map((item) => (
+            <OurBlogCard key={item.id} {...item} />
+          ))}
+        </div>
+      </Box>
     </div>
   );
 }
